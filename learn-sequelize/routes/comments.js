109ec1f473a56ca22d10af64913a98de05861b3c
anstjaos <vv_eros@naver.com>
2019-10-17 02:07:01 +0900

    MySql exam

---
+++ learn-sequelize/routes/comments.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+// GET /comment
+// GET /comment/:id
+router.get('/:id', (req, res, next) => {
+
+});
+
+router.patch('/:id', (req, res, next) => {
+
+});
+
+router.delete('/:id', (req, res, next) => {
+
+});
+
+router.post('/', (req, res, next) => {
+
+});
+
+module.exports = router;
