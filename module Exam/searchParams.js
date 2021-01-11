8a1de874f9a81f20421e7a2dee886608580196a8
moonseo.kim <moonseo.kim@nhn.com>
2021-01-11 16:55:00 +0900

    modify searchParams typo

--- module Exam/searchParams.js
+++ module Exam/searchParams.js
@@ -1,7 +1,7 @@
 const {URL} = require('url');
 
 const myURL = new URL('https://github.com/?page=3&limit=10&category=nodejs&category=javascript');
-console.log('searchParams: ', myURL.searchParms);
+console.log('searchParams: ', myURL.searchParams);
 
 console.log('searchParams.getAll(): ', myURL.searchParams.getAll('category'));
 console.log('searchParams.get(): ', myURL.searchParams.get('limit'));
@@ -10,6 +10,7 @@ console.log('searchParams.keys(): ', myURL.searchParams.keys());
 console.log('searchParams.values(): ', myURL.searchParams.values());
 
 myURL.searchParams.append('filter', 'es3');
+myURL.searchParams.append('filter', 'es5');
 console.log(myURL.searchParams.getAll('filter'));
 
 console.log('searchParams.toString()', myURL.searchParams.toString());
