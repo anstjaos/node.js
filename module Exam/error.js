ba9c8dba242eac907ad086b7cd0efbbd750db4bf
moonseo.kim <moonseo.kim@nhn.com>
2021-01-13 17:42:54 +0900

    add some code and comments

--- module Exam/error.js
+++ module Exam/error.js
@@ -9,6 +9,10 @@ const fs = require('fs');
 //     });
 //     console.log('끝');
 // }, 1000);
+
+// 모든 에러를 처리할 수 있지만
+// uncaughtException에 의존하지 말고 근본적인 에러의 원인을 해결해야한다.
+// node는 process.on 콜백의 실행을 항상 동작한다고 보장하지 않는다.
 process.on('uncaughtException', (err) => {
     console.error('예기치 못한 에러', err);
 });
