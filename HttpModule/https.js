8ef954c9202c04ead884739f09ebe6e78f85064c
moonseo.kim <moonseo.kim@nhn.com>
2021-01-18 17:59:50 +0900

    add comment

--- HttpModule/https.js
+++ HttpModule/https.js
@@ -2,6 +2,7 @@ const https = require('https');
 const fs = require('fs');
 const http2 = require('http2');
 
+// lets encrypt 무료 인증서를 발급받았다고 가정.
 https.createServer({
     cert: fs.readFileSync('도메인 인증서 경로'),
     key: fs.readFileSync('도메인 비밀키 경로'),
