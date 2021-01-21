d25739c7866046d575fe7eed5745abf7aa88a745
moonseo.kim <moonseo.kim@nhn.com>
2021-01-21 17:44:49 +0900

    add comment

--- learn-express2/app.js
+++ learn-express2/app.js
@@ -30,7 +30,7 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
+  res.locals.message = err.message; // 다른 미들웨어 안에서도 선언할 수 있다.
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
