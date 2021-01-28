24d3474f59a53a25b3e9774e44f719e1bb12458e
moonseo.kim <moonseo.kim@nhn.com>
2021-01-28 15:47:29 +0900

    add revenue, expense, balance code

--- node-cli/wallet.js
+++ node-cli/wallet.js
@@ -3,6 +3,7 @@ const program = require('commander');
 const inquirer = require('inquirer');
 
 const { version } = require('./package.json');
+const { sequelize, Wallet } = require('./models');
 
 program
     .version(version, '-v, --version')
@@ -12,17 +13,44 @@ program
 program
     .command('revenue <money> <desc>')
     .description('수입을 기록합니다.')
-    .action(() => {});
+    .action(async (money, desc) => {
+        await sequelize.sync();
+        await Wallet.create({
+            money: parseInt(money, 10),
+            desc,
+            type: true,
+        });
+        console.log(`${money}원을 얻었습니다.`);
+        await sequelize.close();
+    });
 // 지출
 program
     .command('expense <money> <desc>')
     .description('지출을 기록합니다.')
-    .action(() => {});
+    .action(async (money, desc) => {
+        await sequelize.sync();
+        await Wallet.create({
+            money: parseInt(money, 10),
+            desc,
+            type: false,
+        });
+        console.log(`${money}원을 썼습니다.`);
+        await sequelize.close();
+    });
 // 잔액
 program
     .command('balance')
     .description('잔액을 표시합니다.')
-    .action(() => {});
+    .action(async () => {
+        await sequelize.sync();
+        const logs = await Wallet.findAll({});
+        const revenue = logs.filter(log => log.type === true)
+            .reduce((acc, cur) => acc + cur.money, 0);
+        const expense = logs.filter(log => log.type === false)
+            .reduce((acc, cur) => acc + cur.money, 0);
+        console.log(`${revenue - expense}원 남았습니다.`);
+        await sequelize.close();
+    });
 
 program
     .command('*')
