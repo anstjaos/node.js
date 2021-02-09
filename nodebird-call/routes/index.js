7eb305780a0e8ad0a7a9cfb9eb7cb2d90c1ac196
moonseo.kim <moonseo.kim@nhn.com>
2021-02-09 16:26:17 +0900

    get post and hashtags

--- nodebird-call/routes/index.js
+++ nodebird-call/routes/index.js
@@ -21,10 +21,52 @@ router.get('/test', async (req, res, next) => {
         return res.json(result.data);
     } catch (error) {
         console.error(error);
-        if (error.response.status === 419) {
+        if (error.response.status === 419) {    // 토큰 만료 에러
             return res.json(error.response.data);
         }
         return next(error);
+    }
+});
+
+const request = async (req, api) => {
+    try {
+        if (!req.session.jwt) {
+            const tokenResult = await axios.post('http://localhost:8002/v1/token', {
+                clientSecret: process.env.CLIENT_SECRET,
+            });
+            req.session.jwt = tokenResult.data.token;
+        }
+        return await axios.get(`http://localhost:8002/v1${api}`, {
+            headers: { authorization: req.session.jwt },
+        });
+    } catch (error) {
+        console.error(error);
+        if (error.response.status < 500) {
+            return error.response;
+        }
+        throw error;
+    }
+};
+
+router.get('/mypost', async (req, res, next) => {
+    try {
+        const result = await request(req, '/posts/my');
+        res.json(result.data);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.get('/search/:hashtag', async (req, res, next) => {
+    try {
+        const result = await request(
+          req, `/posts/hashtag/${encodeURIComponent(req.params.hashtag)}`,
+        );
+        res.json(result.data);
+    } catch (error) {
+        console.error(error);
+        next(error);
     }
 });
 
