a64b06d92940bdb64531c7e45546f69ccb216aad
moonseo.kim <moonseo.kim@nhn.com>
2021-02-17 17:13:35 +0900

    modify settings

--- gif-chat/routes/index.js
+++ gif-chat/routes/index.js
@@ -8,11 +8,7 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
    try {
       const rooms = await Room.find({});
-      res.render('main', {
-         rooms,
-         title: 'GIF 채팅방',
-         error: req.flash('roomError')
-      });
+      res.render('main', { rooms, title: 'GIF 채팅방' });
    } catch (error) {
       console.error(error);
       next(error);
@@ -20,7 +16,7 @@ router.get('/', async (req, res, next) => {
 });
 
 router.get('/room', (req, res) => {
-   res.render('room', { title: 'GIF 채팅방 생성'});
+   res.render('room', { title: 'GIF 채팅방 생성' });
 });
 
 router.post('/room', async (req, res, next) => {
@@ -40,24 +36,20 @@ router.post('/room', async (req, res, next) => {
    }
 });
 
-router.get('/room/:id', async(req, res, next) => {
+router.get('/room/:id', async (req, res, next) => {
    try {
       const room = await Room.findOne({ _id: req.params.id });
       const io = req.app.get('io');
       if (!room) {
-         req.flash('roomError', '존재하지 않는 방입니다.');
-         return res.redirect('/');
+         return res.redirect('/?error=존재하지 않는 방입니다.');
       }
       if (room.password && room.password !== req.query.password) {
-         req.flash('roomError', '비밀번호가 틀렸습니다.');
-         return res.redirect('/');
+         return res.redirect('/?error=비밀번호가 틀렸습니다.');
       }
       const { rooms } = io.of('/chat').adapter;
-      if (rooms && rooms[req.params.id] && rooms.max <= rooms[req.params.id]) {
-         req.flash('roomError', '허용 인원 초과');
-         return res.redirect('/');
+      if (rooms && rooms[req.params.id] && room.max <= rooms[req.params.id].length) {
+         return res.redirect('/?error=허용 인원이 초과하였습니다.');
       }
-
       return res.render('chat', {
          room,
          title: room.title,
@@ -66,7 +58,22 @@ router.get('/room/:id', async(req, res, next) => {
       });
    } catch (error) {
       console.error(error);
+      return next(error);
+   }
+});
+
+router.delete('/room/:id', async (req, res, next) => {
+   try {
+      await Room.remove({ _id: req.params.id });
+      await Chat.remove({ room: req.params.id });
+      res.send('ok');
+      setTimeout(() => {
+         req.app.get('io').of('/room').emit('removeRoom', req.params.id);
+      }, 2000);
+   } catch (error) {
+      console.error(error);
       next(error);
    }
-})
+});
+
 module.exports = router;
--- gif-chat/socket.js
+++ gif-chat/socket.js
@@ -1,40 +1,15 @@
-//const WebSocket = require('ws');
 const SocketIO = require('socket.io');
 const axios = require('axios');
 const cookieParser = require('cookie-parser');
+const cookie = require('cookie-signature');
 
 module.exports = (server, app, sessionMiddleware) => {
-    // const wss = new WebSocket.Server({ server });
-    //
-    // wss.on('connection', (ws, req) => {
-    //     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    //     console.log('클라이언트 접속', ip);
-    //     ws.on('message', (message) => {
-    //        console.log(message);
-    //     });
-    //     ws.on('error', (error) => {
-    //        console.error(error);
-    //     });
-    //     ws.on('close', () => {
-    //        console.log('클라이언트 접속 해제', ip);
-    //        clearInterval(ws.interval);
-    //     });
-    //     const interval = setInterval(() => {
-    //         if (ws.readyState === ws.OPEN) {
-    //             ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
-    //         }
-    //     }, 3000);
-    //     ws.interval = interval;
-    // });
-
     const io = SocketIO(server, { path: '/socket.io' });
-    app.set('io', io); // 익스프레스 변수 저장 방법
-    // req.app.get('io').of('/room').emit
-    // 네임 스페이스
-    // default: io.of('/');
+    app.set('io', io);
     const room = io.of('/room');
     const chat = io.of('/chat');
-    io.use((socket, next) => {  // 익스프레스 미들웨어 사용 방법
+
+    io.use((socket, next) => {
         cookieParser(process.env.COOKIE_SECRET)(socket.request, socket.request.res, next);
         sessionMiddleware(socket.request, socket.request.res, next);
     });
@@ -54,7 +29,6 @@ module.exports = (server, app, sessionMiddleware) => {
             .split('/')[referer.split('/').length - 1]
             .replace(/\?.+/, '');
         socket.join(roomId);
-
         socket.to(roomId).emit('join', {
             user: 'system',
             chat: `${req.session.color}님이 입장하셨습니다.`,
@@ -64,10 +38,15 @@ module.exports = (server, app, sessionMiddleware) => {
             console.log('chat 네임스페이스 접속 해제');
             socket.leave(roomId);
             const currentRoom = socket.adapter.rooms[roomId];
-            const userCount = currentRoom ? currentRoom.length: 0;
-
-            if (userCount === 0) {
-                axios.delete(`http://localhost:8005/room/${roomId}`)
+            const userCount = currentRoom ? currentRoom.length : 0;
+            if (userCount === 0) { // 유저가 0명이면 방 삭제
+                const signedCookie = cookie.sign( req.signedCookies['connect.sid'], process.env.COOKIE_SECRET );
+                const connectSID = `${signedCookie}`;
+                axios.delete(`http://localhost:8005/room/${roomId}`, {
+                    headers: {
+                        Cookie: `connect.sid=s%3A${connectSID}`
+                    }
+                })
                     .then(() => {
                         console.log('방 제거 요청 성공');
                     })
@@ -81,24 +60,5 @@ module.exports = (server, app, sessionMiddleware) => {
                 });
             }
         });
-    });
-
-    io.on('connection', (socket) => {
-        const req = socket.request;
-        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        console.log('새로운 클라이언트 접속!', ip, socket.id, req.ip);
-        socket.on('disconnect', () => {
-            console.log('클라이언트 접속 해제', ip, socket.id);
-            clearInterval(socket.interval);
-        });
-        socket.on('error', (error) => {
-            console.error(error);
-        });
-        socket.on('reply', (data) => {
-            console.log(data);
-        });
-        socket.interval = setInterval(() => {
-            socket.emit('news', 'Hello Socket.IO'); // key, value
-        }, 3000);
     });
 };
