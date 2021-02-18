8807dcfd33051433de4a6d89c09fa693b7f60d7c
moonseo.kim <moonseo.kim@nhn.com>
2021-02-18 15:13:58 +0900

    add socket and sse

---
+++ node-auction/socket.js
@@ -0,0 +1,16 @@
+module.exports = (server, app) => {
+    const io = require('socket.io')(server, { path: '/socket.io'});
+
+    app.set('io', io);
+
+    io.on('connection', (socket) => {
+        const req = socket.request;
+        const { headers: { referer }} = req;
+        // req.cookies, req.session 접근 불가, 접근하려면 io.use 미들웨어를 연
+        const roomId = referer.split('/')[referer.split('/').length - 1];결
+        socket.join(roomId);
+        socket.on('disconnect', () => {
+            socket.leave(roomId);
+        });
+    });
+};
