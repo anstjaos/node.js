cb70a0f32c0c8f1d70f097eee6e48ab344f5791d
moonseo.kim <moonseo.kim@nhn.com>
2021-02-19 14:57:13 +0900

    add auction bid function

--- node-auction/socket.js
+++ node-auction/socket.js
@@ -6,8 +6,8 @@ module.exports = (server, app) => {
     io.on('connection', (socket) => {
         const req = socket.request;
         const { headers: { referer }} = req;
-        // req.cookies, req.session 접근 불가, 접근하려면 io.use 미들웨어를 연
-        const roomId = referer.split('/')[referer.split('/').length - 1];결
+        // req.cookies, req.session 접근 불가, 접근하려면 io.use 미들웨어를 연결
+        const roomId = referer.split('/')[referer.split('/').length - 1];
         socket.join(roomId);
         socket.on('disconnect', () => {
             socket.leave(roomId);
